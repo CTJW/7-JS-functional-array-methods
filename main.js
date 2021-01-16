31457d4cf3df2fdf25edd8bcab9486d195426bc0
Candice James-Waples <76015275+CTJW@users.noreply.github.com>
2021-01-15 21:57:37 -0500

    revist

---
+++ main.js
@@ -0,0 +1,160 @@
+
+// QUESTION 1
+
+const totalNumberOfItems = items.length
+
+let total = 0
+
+// items.forEach(function(item){
+    // const itemPrice = item.Price
+    // console.log(total, itemPrice)
+    // total = total + itemPrice
+// })
+
+items.forEach (function (item) {
+})
+
+const avgPrice = total/totalNumberOfItems
+// console.log(avgPrice)
+
+document.querySelector("#answer1").innerHTML = `${totalNumberOfItems}`
+
+// QUESTION 2
+// 3 parts!
+
+
+// Logic Part 1
+// Have a variable called priceOfItems - 
+// filtering through all the items to find which ones are priced between $14-$18. 
+// Returns all the items that are priced from $14-$18. Which is 3 items
+const priceOfItems = items.filter (function(item){
+     if (item.price > 14 && item.price < 18){
+         return true
+        }
+    })
+
+// By this point priceOfItems will be 3
+// console.log(priceOfItems)
+
+// Logic Part 2
+// We have to map through priceOfItems (which has 3 items) and now we want to
+// display them in a list in our HTML
+
+// Created a new variable called priceOfItemsTitle to hold all of the item titles
+const priceOfItemsTitle = items.map(function (item) {
+    return `<li>${item.title}</li>`
+}).join("")
+
+// this should give us the list of item TITLES that are priced between $14-$18
+
+
+// Now we have the item titles (there's 3 of them) now we have to actually display them
+// in our HTML
+// Displays the titles using querySelectors
+
+// Now we use the priceOfItemsTitle variable in our innerHTML
+document.querySelector("#answer2").innerHTML = `${priceOfItemsTitle}`
+
+
+
+
+// QUESTION 3
+// Which item has a "GBP" currency code? Display it's name and price.
+
+const gbp = items.filter(function(item) {
+    let gbpItem = item.GBP
+    return `<li>${item.title} ${item.price}</li>`
+})
+
+
+
+//     if (item.currency_code == "GBP"){
+//        return `<li>${item.title} ${item.price}</li>`
+//     }
+// }) 
+
+// const gbpItem = items.map(function(item) {
+//    return `<li>${item.title} ${item.price}</li>`
+// })
+
+document.querySelector("#answer3").innerHTML = `${gbpItem}`
+
+
+// if code is gbp (item.currency_code == "GBP")
+// return title and price in a <li>{item.title} {item.price}</li>
+// .join("")
+
+//display the answer 
+
+
+
+// QUESTION 4
+// Display a list of all items who are made of wood.
+
+const woodItem = items.filter(function(item){
+    if (item.materials == "wood" ){
+        return `<li> ${item.title} </li>`
+    }
+}) .join("")
+document.querySelector("#answer4").innerHTML = `${woodItem}`
+
+
+
+
+// QUESTION 5
+// Which items are made of eight or more materials? 
+// Display the name, number of items and the items it is made of.
+
+const numOfMaterials = items.filter (function(item){
+    if(item.materials >= 8){
+        return `<li> ${item.title} ${item.materials} ${item.materials.length} </li>`
+
+    }
+}) .join("") 
+document.querySelector("#answer5").innerHTML = `${numOfMaterials}`
+
+
+
+
+// QUESTION 6
+// How many items were made by their sellers?
+
+const sellers = items.filter(function(item){
+    if (item.who_made == "i_did"){
+        return `<li> ${item.length} </li>`
+    }
+}) .join("")
+document.querySelector("#answer6").innerHTML = `${sellers}`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
