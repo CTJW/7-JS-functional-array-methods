632775ac080023f7f4317f93998fc640a332c050
Candice James-Waples <76015275+CTJW@users.noreply.github.com>
2021-01-17 16:57:46 -0500

    completed assignment for once

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 // QUESTION 1
+// What is the average price of all items?
 
 const totalNumberOfItems = items.length
 
@@ -20,6 +21,7 @@ const avgPrice = total/totalNumberOfItems
 document.querySelector("#answer1").innerHTML = `${totalNumberOfItems}`
 
 // QUESTION 2
+// Show me an array of items that cost between $14.00 and $18.00 USD?
 // 3 parts!
 
 
@@ -28,20 +30,18 @@ document.querySelector("#answer1").innerHTML = `${totalNumberOfItems}`
 // filtering through all the items to find which ones are priced between $14-$18. 
 // Returns all the items that are priced from $14-$18. Which is 3 items
 const priceOfItems = items.filter (function(item){
-     if (item.price > 14 && item.price < 18){
-         return true
-        }
+     return item.price > 14 && item.price < 18
     })
 
 // By this point priceOfItems will be 3
-// console.log(priceOfItems)
+
 
 // Logic Part 2
 // We have to map through priceOfItems (which has 3 items) and now we want to
 // display them in a list in our HTML
 
 // Created a new variable called priceOfItemsTitle to hold all of the item titles
-const priceOfItemsTitle = items.map(function (item) {
+const priceOfItemsTitle = priceOfItems.map(function (item) {
     return `<li>${item.title}</li>`
 }).join("")
 
@@ -61,23 +61,18 @@ document.querySelector("#answer2").innerHTML = `${priceOfItemsTitle}`
 // QUESTION 3
 // Which item has a "GBP" currency code? Display it's name and price.
 
-const gbp = items.filter(function(item) {
-    let gbpItem = item.GBP
-    return `<li>${item.title} ${item.price}</li>`
-})
+ const gbp = items.filter(function(item) {
+   return item.currency_code == "GBP" 
+ })
+
+// const gbpItem = 
 
 
+   const gbpItem = gbp.map(function(item) {
+      return `<li>${item.title} ${item.price}</li>`
+ })
 
-//     if (item.currency_code == "GBP"){
-//        return `<li>${item.title} ${item.price}</li>`
-//     }
-// }) 
-
-// const gbpItem = items.map(function(item) {
-//    return `<li>${item.title} ${item.price}</li>`
-// })
-
-document.querySelector("#answer3").innerHTML = `${gbpItem}`
+ document.querySelector("#answer3").innerHTML = `${gbpItem}`
 
 
 // if code is gbp (item.currency_code == "GBP")
@@ -91,12 +86,15 @@ document.querySelector("#answer3").innerHTML = `${gbpItem}`
 // QUESTION 4
 // Display a list of all items who are made of wood.
 
-const woodItem = items.filter(function(item){
-    if (item.materials == "wood" ){
-        return `<li> ${item.title} </li>`
-    }
-}) .join("")
-document.querySelector("#answer4").innerHTML = `${woodItem}`
+ const woodItem = items.filter(function(item){
+      return item.materials.includes ("wood")
+ }) 
+
+const woody = woodItem.map(function(item){
+    return `<li> ${item.title} ${"is made of wood"} </li>`
+}).join("") 
+ 
+document.querySelector("#answer4").innerHTML = `${woody}`
 
 
 
@@ -106,25 +104,29 @@ document.querySelector("#answer4").innerHTML = `${woodItem}`
 // Display the name, number of items and the items it is made of.
 
 const numOfMaterials = items.filter (function(item){
-    if(item.materials >= 8){
-        return `<li> ${item.title} ${item.materials} ${item.materials.length} </li>`
+     return item.materials.length >= 8 
+   })
+    // console.log(numOfMaterials)
 
-    }
-}) .join("") 
-document.querySelector("#answer5").innerHTML = `${numOfMaterials}`
+    const materials = numOfMaterials.map (function(item){
+        return `<li> ${item.title} ${item.materials.length} ${item.materials}  </li>`
+
+    }).join("") 
+ document.querySelector("#answer5").innerHTML = `${materials}`
 
 
 
 
-// QUESTION 6
-// How many items were made by their sellers?
+//  QUESTION 6
+//  How many items were made by their sellers?
 
-const sellers = items.filter(function(item){
-    if (item.who_made == "i_did"){
-        return `<li> ${item.length} </li>`
-    }
-}) .join("")
-document.querySelector("#answer6").innerHTML = `${sellers}`
+ const sellers = items.filter(function(item){
+    return item.who_made.includes("i_did")
+     })
+
+const sellersNum = sellers.length
+
+document.querySelector("#answer6").innerHTML = `${sellersNum}`
 
 
 
